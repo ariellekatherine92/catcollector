404c67075404767127d95c6fb011778fb5acc6fa
“ariellekatherine92” <ariellekatherine92@gmail.com>
2021-06-23 16:31:36 -0400

    Seventh Commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Cat
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
 # Create your views here.
 def about(request):
     return render(request, 'about.html')
@@ -30,5 +32,23 @@ def cats_show(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     data = {'cat': cat }
     return render(request, 'cats/show.html', data)
+
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+  success_url = '/cats'
+
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['name', 'breed', 'description', 'age']
+
+  def form_valid(self, form):
+    self.object = form.save(commit=False)
+    self.object.save()
+    return HttpResponseRedirect('/cats/' + str(self.object.pk))
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats'
 
 
